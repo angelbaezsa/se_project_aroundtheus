93ae0c98babffaa6ceef8cc49ebbbce1ef15add9
Mona Lisa <angelbaezsa@gmail.com>
2023-03-21 12:27:54 -0400

    code refactor

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # Project 3: Around The U.S.
 
-### Overview  
+### Overview
 
-* Intro  
-* Figma  
-* Images  
-  
+- Intro
+- Figma
+- Images
+
 **Intro**
-  
-This project is made so all the elements are displayed correctly on popular screen sizes. 
+
+This project is made so all the elements are displayed correctly on popular screen sizes.
 
 The main purpose of this project is to use display flex and grid properties to achieve a beautiful adaptative web page.
 
+## version 0.2
+
+now cards are created and rendered using JS, functions and forms were added.
 
 LINK TO DEPLOYED WEBSITE: |
 https://angelbaezsa.github.io/se_project_aroundtheus/
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -24,30 +24,35 @@ let initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
-// this code generates the gallery cards using JS and template
-let cardTemplate = document.querySelector("#card").content;
-let Gallery = document.querySelector(".gallery");
+
+// this code generates the gallery cards using JS and templatemodal-box__button-close
+const cardTemplate = document.querySelector("#card").content;
+const gallery = document.querySelector(".gallery");
 
 function createCards() {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  return cardElement;
+}
+function renderCards() {
   for (let i = 0; i < initialCards.length; i++) {
-    let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    const cardElement = createCards();
     cardElement.querySelector(".card__title").textContent =
       initialCards[i].name;
     cardElement.querySelector(".card__image").src = initialCards[i].link;
-    Gallery.append(cardElement);
+    gallery.append(cardElement);
   }
 }
-createCards();
+renderCards();
 
 //----------------------------------------------------------------------------------------------------->
-const closeButton = document.querySelector(".form__button-close"); //button that closes form
+const closeButton = document.querySelector(".modal-box__button-close"); //button that closes form
 const editButton = document.querySelector(".profile__edit-button"); //button that opens form
 const saveButton = document.querySelector(".form__save-button");
 const form = document.querySelector(".form"); //button that saves changes in the profile
 
 closeButton.addEventListener("click", closeModal);
 editButton.addEventListener("click", openModal);
-saveButton.addEventListener("click", updateProfile);
+form.addEventListener("submit", updateProfile);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -65,33 +70,30 @@ function getOccupation() {
   return occupation;
 }
 
-function formFiller(name, occupation) {
-  console.log(name, occupation);
-  document.querySelector(".form__input-name").value = name;
-  document.querySelector(".form__input-description").value = occupation;
+function fillForm(name, occupation) {
+  name = document.querySelector(".form__input-name").value = name;
+  occupation = document.querySelector(".form__input-description").value =
+    occupation;
 }
 
 // closes modal div that contains form
 function closeModal() {
-  var form = document.querySelector(".modal-box");
-  form.setAttribute("style", "visibility: hidden");
+  const form = document.querySelector(".modal-box");
+  form.classList.toggle("modal-box_visible");
 }
 // opens modal div that contains form
 function openModal() {
-  var form = document.querySelector(".modal-box");
-  form.setAttribute("style", "visibility: visible");
-  formFiller(getName(), getOccupation());
+  const form = document.querySelector(".modal-box");
+  form.classList.toggle("modal-box_visible");
+  fillForm(getName(), getOccupation());
 }
 
 // updates profile with new values if the input fields are not empty
 function updateProfile() {
   let newName = document.querySelector(".form__input-name").value;
   let newOccupation = document.querySelector(".form__input-description").value;
-
-  if (newName && newOccupation != "") {
-    document.querySelector(".profile__heading").textContent = newName;
-    document.querySelector(".profile__sub-heading").textContent = newOccupation;
-  }
+  document.querySelector(".profile__heading").textContent = newName;
+  document.querySelector(".profile__sub-heading").textContent = newOccupation;
 
   closeModal();
 }
