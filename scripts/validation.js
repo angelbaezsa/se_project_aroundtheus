9ba84b39cfc9703c25f6de058901198d7ffe8f38
angelbaezsa <angelbaezsa@gmail.com>
2023-05-02 10:32:19 -0400

    functionality among form buttons added, now they get deactivated if form has invalid inputs

--- scripts/validation.js
+++ scripts/validation.js
@@ -9,14 +9,27 @@ const config = {
 
 const formElements = [...document.querySelectorAll(".form")];
 const formInputs = [...document.querySelectorAll(".form__input")];
+const formSaveButtons = document.querySelectorAll(".form__button");
 
 function checkInputValidity(input, formElement) {
   const txtField = input;
   if (!input.validity.valid) {
     document.querySelector(`.${input.name}_error-message`).textContent =
       input.validationMessage;
+    const form = document.querySelector(
+      `.${input.name}_error-message`
+    ).parentElement;
+    const currentButton = form.querySelector(".form__save-button");
+    currentButton.disabled = true;
+    currentButton.classList.add("form__button_disabled");
   } else {
     document.querySelector(`.${input.name}_error-message`).textContent = "";
+    const form = document.querySelector(
+      `.${input.name}_error-message`
+    ).parentElement;
+    const currentButton = form.querySelector(".form__save-button");
+    currentButton.disabled = false;
+    currentButton.classList.remove("form__button_disabled");
   }
 }
 
