f50e8df45e9963bb952d0646f37552e703a74a79
angelbaezsa <angelbaezsa@gmail.com>
2023-05-02 14:26:40 -0400

    corrected BEM class name

--- scripts/validation.js
+++ scripts/validation.js
@@ -14,18 +14,18 @@ const formSaveButtons = document.querySelectorAll(".form__button");
 function checkInputValidity(input, formElement) {
   const txtField = input;
   if (!input.validity.valid) {
-    document.querySelector(`.${input.name}_error-message`).textContent =
+    document.querySelector(`.${input.name}__error-message`).textContent =
       input.validationMessage;
     const form = document.querySelector(
-      `.${input.name}_error-message`
+      `.${input.name}__error-message`
     ).parentElement;
     const currentButton = form.querySelector(".form__save-button");
     currentButton.disabled = true;
     currentButton.classList.add("form__button_disabled");
   } else {
-    document.querySelector(`.${input.name}_error-message`).textContent = "";
+    document.querySelector(`.${input.name}__error-message`).textContent = "";
     const form = document.querySelector(
-      `.${input.name}_error-message`
+      `.${input.name}__error-message`
     ).parentElement;
     const currentButton = form.querySelector(".form__save-button");
     currentButton.disabled = false;
